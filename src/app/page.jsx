132b841ff238e235dfef375eb4a17527ae3f400d
Wakki <101950814+ryo28@users.noreply.github.com>
2023-11-06 01:56:38 +0900

    Initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex h-20 w-20 items-center justify-center rounded-full bg-blue-500 text-red-500">
+        テスト
+      </div>
+    </>
+  );
+}
